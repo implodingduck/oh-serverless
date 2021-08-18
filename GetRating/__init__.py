9edb728d39c8a5eaadefdeeee347dfba2b0bccb9
implodingduck <zsek320@gmail.com>
2021-08-18 11:38:40 -0500

    see what explodes next

--- GetRating/__init__.py
+++ GetRating/__init__.py
@@ -1,15 +1,15 @@
 import logging
 
 import azure.functions as func
-from azure.cosmos import CosmosClient
 import os 
 import json 
 
 def main(req: func.HttpRequest, doc: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    for d in doc:
+        logging.info(d)
     ratingId = req.route_params.get('ratingId')
-    print(ratingId)
-    print(f'{doc}')
-    return func.HttpResponse(f"{json.dumps(doc)}", status_code=200)
+    logging.info(ratingId)
+    logging.info(f'{doc}')
+    return func.HttpResponse(f"{doc}", status_code=200)
     
