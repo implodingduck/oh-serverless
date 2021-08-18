4e327629682d591aa13fdc6557c83d456434777a
implodingduck <zsek320@gmail.com>
2021-08-18 11:14:08 -0500

    let see if this method works

--- GetRating/__init__.py
+++ GetRating/__init__.py
@@ -5,22 +5,11 @@ from azure.cosmos import CosmosClient
 import os 
 import json 
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, doc: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     ratingId = req.route_params.get('ratingId')
-    connection_str = os.environ.get('AzureCosmosDBConnectionString')
-    client = CosmosClient(connection_str)
-    database_name = 'bfyocproductrating'
-    database = client.get_database_client(database_name)
-    container_name = 'ProductRating'
-    container = database.get_container_client(container_name)
-    query = 'SELECT * FROM ProductRating r WHERE r.id="{ratingId}"'
-    retVal = {}
-    for item in container.query_items(query=query, enable_cross_partition_query=True):
-        print(f'{item}')
-        retVal = item
-
-    
-    return func.HttpResponse(f"{json.dumps(retVal)}", status_code=200)
+    print(ratingId)
+    print(f'{doc}')
+    return func.HttpResponse(f"{json.dumps(doc)}", status_code=200)
     
