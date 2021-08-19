fe783586deec754b4ebeeb12d895f0bb777cdcf7
implodingduck <zsek320@gmail.com>
2021-08-19 12:18:27 -0500

    making it look like the example

--- EventGridOrders/__init__.py
+++ EventGridOrders/__init__.py
@@ -4,7 +4,7 @@ import logging
 import azure.functions as func
 import azure.durable_functions as df
 
-def main(event: func.EventGridEvent, starter: str):
+async def main(event: func.EventGridEvent, starter: str):
     result = json.dumps({
         'id': event.id,
         'data': event.get_json(),
@@ -15,5 +15,6 @@ def main(event: func.EventGridEvent, starter: str):
     logging.warning(f"Starter is {starter}")
     client = df.DurableOrchestrationClient(starter)
     entity_id = df.EntityId("Counter", "myCounter")
-    client.signal_entity(entity_id, "add", 1)
+    something = await client.signal_entity(entity_id, "add", 1)
     logging.info('Python EventGrid trigger processed an event: %s', result)
+    logging.warning(f'{something}')
