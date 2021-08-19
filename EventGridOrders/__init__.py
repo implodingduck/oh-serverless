c4837c1fea25f6accd2352cd6b58c0a55b496c45
implodingduck <zsek320@gmail.com>
2021-08-19 12:06:55 -0500

    lets make the function talk to the durable

--- EventGridOrders/__init__.py
+++ EventGridOrders/__init__.py
@@ -2,9 +2,9 @@ import json
 import logging
 
 import azure.functions as func
+import azure.durable_functions as df
 
-
-def main(event: func.EventGridEvent):
+def main(event: func.EventGridEvent, starter: str):
     result = json.dumps({
         'id': event.id,
         'data': event.get_json(),
@@ -12,5 +12,8 @@ def main(event: func.EventGridEvent):
         'subject': event.subject,
         'event_type': event.event_type,
     })
-
+    logging.warning(f"Starter is {starter}")
+    client = df.DurableOrchestrationClient(starter)
+    entity_id = df.EntityId("Counter", "myCounter")
+    client.signal_entity(entity_id, "add", 1)
     logging.info('Python EventGrid trigger processed an event: %s', result)
